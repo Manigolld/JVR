da5308ade7e2cc93e3c1bd9772435af82e57a027
Manigolld <rjoaovictor898@gmail.com>
2022-10-24 20:06:57 -0300

    Fixed the "Duck" method

--- dino_runner/components/dinossaur.py
+++ dino_runner/components/dinossaur.py
@@ -38,12 +38,14 @@ class Dinossaur:
 
     def update(self, user_input):
         
+        
         if self.dino_duck:
             self.duck()
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump =True
@@ -52,11 +54,11 @@ class Dinossaur:
             self.dino_jump = False
             self.dino_run = True
         
-        if user_input[pygame.K_DOWN]:   
+        if user_input[pygame.K_DOWN] and not self.dino_jump:   
             self.dino_duck = True
             self.dino_run = False           
-            self.dino_rect.y = 350
-
+            
+            
         if self.step_index >= 10:
             self.step_index = 0
         
@@ -75,16 +77,21 @@ class Dinossaur:
 
     def duck(self):
 
-        self.image = DUCKING[1]
+        self.image = DUCKING
 
         if self.step_index < 5:
-            self.image = DUCKING[1]
-        else:
             self.image = DUCKING[0]
+        else:
+            self.image = DUCKING[1]
 
         self.step_index+=1
- 
 
+        if self.dino_duck:
+            self.dino_rect.y = 340
+            self.dino_rect.x = X_POS
+ 
+        self.dino_duck = False
+        
     def draw(self, screen):
         
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
